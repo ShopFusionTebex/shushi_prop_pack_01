744cd2c730de64981b44c0c74cbf368e3953f577
ShopFusionTebex <mariusz.karauda@gmail.com>
2024-09-19 16:05:29 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'ShopFusion (https://shopfusion.tebex.io/)'
+description 'Sushi Pack'
+version '1.0.0'
+lua54 'yes'
+this_is_a_map 'yes'
+
+
+
+
+dependency '/assetpacks'
